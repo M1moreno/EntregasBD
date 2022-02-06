250d9a9caa0f12f62004d1b5cd90d7e9d8dbe069
Michael Moreno Valoyes <80339158+M1moreno@users.noreply.github.com>
2022-02-06 18:53:39 -0500

    Added final zip

--- Trabajo4/trabajo4/configuraciones/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'localhost';
-$user = "root";
-$pass = "";
-$DB = "nidofaust";
-$conn = new mysqli($host, $user, $pass, $DB) or die("Error al conectar a la DB " . mysqli_error($link));
-?>
